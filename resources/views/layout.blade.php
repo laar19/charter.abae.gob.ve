8d8256df17c8acfda812ff1be1eeccacc39cb8a8
Luis Alejandro <laar@protonmail.com>
2020-12-15 13:12:18 -0400

    Modify assets call

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,12 +4,11 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title></title>
-        <link rel="stylesheet" href="">
         
-        <link rel="stylesheet" href="/assets/css/main.css">
+        <link href="{{ asset('/assets/css/main.css') }}" rel="stylesheet">
         
         <!-- Bootstrap Core CSS -->
-        <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
+        <link href="{{ asset('/assets/css/bootstrap.min.css') }}" rel="stylesheet">
 
         <!-- Custom Fonts -->
         <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
@@ -17,16 +16,16 @@
 
     <body>
         <header>
-            <center><img src="/assets/img/cintillo-julio-2017.png"></center>
+            <center><img src="{{ asset('/assets/img/cintillo-julio-2017.png') }}"></center>
         </header>
         <br><br><br>
 
         @yield('content')
 
         <!-- jQuery -->
-        <script src="/assets/js/jquery/jquery-3.2.1.min.js"></script>
+        <script src="{{ asset('/assets/js/jquery/jquery-3.2.1.min.js') }}"></script>
 
         <!-- Bootstrap Core JavaScript -->
-        <script src="/assets/js/bootstrap.min.js"></script>
+        <script src="{{ asset('/assets/js/bootstrap.min.js') }}"></script>
     </body>
 </html>
