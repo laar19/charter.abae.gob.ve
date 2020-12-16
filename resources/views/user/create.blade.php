adf95f1e531ce301856a5a2adaee147288108328
Luis Acevedo <laar@protonmail.com>
2020-12-15 20:43:05 -0400

    Minor changes

--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -92,7 +92,9 @@
             </div>
         </center>
 
-        <script src="/assets/js/jquery/jquery-3.2.1.min.js"></script>
-        <script src="/assets/js/jquery/verify_password.js"></script>
+        <!-- jQuery -->
+        <script src="{{ asset('/assets/js/jquery/jquery-3.2.1.min.js') }}"></script>
+
+        <script src="{{ asset('/assets/js/jquery/verify_password.js') }}"></script>
         
 @endsection
